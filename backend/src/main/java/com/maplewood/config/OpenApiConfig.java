c9033dff2c498853af3ed5d1722383b41b7e07eb
CherifYasmine <ycherif@ng-sensors.com>
2026-02-19 21:22:26 +0100

    open api config3

---
+++ backend/src/main/java/com/maplewood/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.maplewood.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Maplewood Course Planning API")
+                .version("1.0.0")
+                .description("REST API for course scheduling, enrollment, and academic metrics")
+                .contact(new Contact()
+                    .name("Maplewood High School")
+                    .url("https://maplewood.edu"))
+            );
+    }
+}
