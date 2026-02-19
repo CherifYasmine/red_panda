18fd5e05f2a7331320c75ef47800e2fde5415b50
CherifYasmine <ycherif@ng-sensors.com>
2026-02-19 22:34:14 +0100

    fixed warnings spring boot version

--- backend/src/main/java/com/maplewood/BackendApplication.java
+++ backend/src/main/java/com/maplewood/BackendApplication.java
@@ -3,9 +3,12 @@ package com.maplewood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.maplewood")
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class BackendApplication {
 
 	public static void main(String[] args) {
